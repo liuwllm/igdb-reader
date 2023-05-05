fb9a2181a35bb2c9d1359f90c9cb5f8c6944149d
liuwllm <liu.wllm@gmail.com>
2023-05-05 12:07:50 -0400

    Make the program output one combined csv instead of multiple csvs + add option to choose games with TBD release date

--- main.py
+++ main.py
@@ -1,75 +1,94 @@
 from igdb.wrapper import IGDBWrapper
-import json
+from decouple import config
 import pandas as pd
+import json
 import ast
 import time
-from decouple import config
 
+# API authentication configuration
 CLIENT_ID = config('CLIENT_ID')
 AUTHORIZATION = config('AUTHORIZATION')
 wrapper = IGDBWrapper(CLIENT_ID, AUTHORIZATION)
 
-csv_columns = ['id', 'involved_companies', 'name', 'release_dates', 'websites']
-
-current_time = 1683225945
 # Current time as unix timestamp; appended to csv name
+current_time = 1683225945
 
-limit_value = 100
 # Total entries per POST request (max is 500)
+limit_value = 500
 
-total_entries = 100
 # total_entries should be divisible by limit value
 # Indicates the total number of entries found overall by running the program once
+total_entries = 1000
 
-alltime_entries = 0
 # Can use as an offset if you don't want to start search from beginning of database
+alltime_entries = 0
 
-for i in range(alltime_entries, total_entries, limit_value):
-    offset = i
+# List to hold all dataframes to be combined later on
+dataframe_list = []
 
+# Checks to see if you want the games outputted to have release dates or have a release date of TBD
+# True = release dates, False = TBD
+want_release_date = False
+if want_release_date:
+    release_date = 'release_dates.date > ' + str(current_time) + '; '
+else:
+    release_date = 'release_dates.category = 7; '
+
+# Sends multiple POST requests using the previously defined parameters
+for offset in range(alltime_entries, total_entries, limit_value):
+
+    # POST request for data on upcoming titles on PC including name, publisher, release date, website links
     post_req = 'fields name,involved_companies.company.name,release_dates.human,websites.url; ' \
-               'where involved_companies.publisher = true & platforms = 6 & release_dates.date > ' + str(current_time) + '; ' \
+               'where involved_companies.publisher = true & platforms = 6 & ' + release_date + '' \
                'sort date asc; ' \
                'limit ' + str(limit_value) + '; ' \
                'offset ' + str(offset) + ';'
-    # POST request for data on upcoming titles on PC including name, publisher, release date, website links
 
+    # Sends POST request to the games endpoint
     byte_array = wrapper.api_request(
         'games',
         post_req
     )
-    # Sends POST request to the games endpoint
 
+    # Decodes byte array returned by API wrapper and converts to JSON
     byte_array = bytearray(byte_array)
     req_dict = ast.literal_eval(byte_array.decode('utf8'))
     json_result = json.dumps(req_dict, indent=4, sort_keys=True)
-    # Decodes byte array returned by API wrapper and converts to JSON
 
-    dataframe = pd.read_json(json_result)
     # Converts JSON to dataframe
+    dataframe = pd.read_json(json_result)
 
-    print(len(dataframe))
-    print(dataframe['websites'][0][1]['url'])
-
+    # Cleans up data by inserting only relevant data points into each cell
     for entry in range(len(dataframe)):
         company_list = []
-        for company in dataframe['involved_companies'][entry]:
-            company_list.append(company['company']['name'])
-        dataframe['involved_companies'][entry] = company_list
+        if type(dataframe['involved_companies'][entry]) == list:
+            for company in dataframe['involved_companies'][entry]:
+                company_list.append(company['company']['name'])
+            dataframe['involved_companies'][entry] = company_list
 
         dates_list = []
-        for date in dataframe['release_dates'][entry]:
-            dates_list.append(date['human'])
-        dataframe['release_dates'][entry] = dates_list
+        if type(dataframe['release_dates'][entry]) == list:
+            for date in dataframe['release_dates'][entry]:
+                dates_list.append(date['human'])
+            dataframe['release_dates'][entry] = dates_list
 
         website_list = []
-        for website in dataframe['websites'][entry]:
-            website_list.append(website['url'])
-        dataframe['websites'][entry] = website_list
+        if type(dataframe['websites'][entry]) == list:
+            for website in dataframe['websites'][entry]:
+                website_list.append(website['url'])
+            dataframe['websites'][entry] = website_list
 
-    csv_name = 'database' + str(time.time()) + '.csv'
-    outfile = open(csv_name, 'wb')
-    dataframe.to_csv(outfile, index=False, header=True, sep=',', encoding='utf-8')
-    outfile.close()
+    # Appends dataframe to the dataframe list
+    dataframe_list.append(dataframe)
+
+# Combines all dataframes into one dataframe
+combined_dataframe = pd.concat(dataframe_list)
+
+# Converts to dataframe to csv
+csv_name = 'database' + str(time.time()) + '.csv'
+outfile = open(csv_name, 'wb')
+combined_dataframe.to_csv(outfile, index=False, header=True, sep=',', encoding='utf-8')
+outfile.close()
+
 
 
